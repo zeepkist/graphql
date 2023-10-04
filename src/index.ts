7fc2d825823119d83f9051d5a6fc752e56799e61
James Harris <3440094+wopian@users.noreply.github.com>
2023-10-04 22:12:40 +0100

    fix: disable query batching
    Signed-off-by: James Harris <3440094+wopian@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import { createClient as createGtr } from './gtr/index.js'
 import { createClient as createZworpshop } from './zworpshop/index.js'
 
 export const gtr = createGtr({
-    batch: true
+    batch: false
 })
 
 export const zworpshop = createZworpshop({
-    batch: true
+    batch: false
 })
